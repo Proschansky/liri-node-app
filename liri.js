1fd89e79a5864ba3eb66afb6be0bbd0c70e06689
Brzeczyszczykiewicz <Samproschansky@gmail.com>
2019-02-02 16:10:55 -0500

    Added a package-lock.

---
+++ liri.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const axios = require('axios');
+const spotify = new Spotify(keys.spotify);
+
+const command = process.argv[2];
+const term = process.argv[3];
+
+
+
+
+
+
+
+
