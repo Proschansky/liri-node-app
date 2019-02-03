1a6a34d02211d870e5664f0cf6abf8f8e9847597
Brzeczyszczykiewicz <Samproschansky@gmail.com>
2019-02-02 20:08:33 -0500

    All functionality is present.

--- keys.js
+++ keys.js
@@ -1,4 +1,6 @@
+console.log('this is loaded');
+
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: '97d088accd7542f38220413eb53828c7',
+  secret: 'b3e864b647d24aeb92995df82710a6f5'
 };
